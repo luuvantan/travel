9c543741f1db6015b1350b525590a852e3d3dde1
TanLV <tanlv@fabbi.io>
2020-06-02 17:31:23 +0700

    base 02062020

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,285 @@
+<div class="n3-header">
+    <div class="h-top">
+        <div class="col-md-4 col-sm-4 col-xs-4">
+            <div class="area-info">
+                <div class="phone-support">
+                    <div class="dropdown-toggle" data-toggle="dropdown">
+                        <div class="f-left">
+                            <span class="hidden-xs">Hotline:</span> <span class="num-color">1900 1839</span>
+                        </div>
+                        <div class="f-left" style="margin-top:-2px;">
+                            <img src="/Content/ThemeHe/img/i-arrow-hl.png" style="margin-left: 8px;" alt="arrow">
+                        </div>
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <table style="width: 100%">
+                                <tbody>
+                                    <tr>
+                                        <td colspan="3" class="city-name">Hồ Ch&#237; Minh</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 53%;">
+                                            Tổng đ&#224;i: <br>
+                                            Du lịch trong nước: <br>
+                                            Du lịch nước ngo&#224;i:<br>
+                                            Dịch vụ kh&#225;ch h&#224;ng:
+                                        </td>
+                                        <td class="whitetext">
+                                            <strong>(028)3822.8898</strong><br>
+                                            <strong>0938.301.399</strong><br>
+                                            <strong>0938.301.388</strong><br>
+                                            <strong>0938.301.234</strong>
+                                        </td>
+                                        <td>
+                                            &nbsp;&nbsp;<br>
+                                            <a href="skype:fit.dltn?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:fit.dlnn?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:online.cskh?chat"><i class="iconT-skype"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3" class="city-name">Chi Nh&#225;nh H&#224; Nội</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 53%;">
+                                            Du lịch trong nước: <br>
+                                            Du lịch nước ngo&#224;i:<br>
+                                            Dịch vụ kh&#225;ch h&#224;ng:
+                                        </td>
+                                        <td class="whitetext">
+                                            <strong>0989.370.033</strong><br>
+                                            <strong>0983.160.022</strong><br>
+                                            <strong>0938.301.234</strong>
+                                        </td>
+                                        <td>
+                                            <a href="skype:fitdltn.hanoi?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:fitdlnn.hanoi?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:online.cskh?chat"><i class="iconT-skype"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3" class="city-name">Chi Nh&#225;nh Đ&#224; Nẵng</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 53%;">
+                                            Du lịch trong nước: <br>
+                                            Du lịch nước ngo&#224;i:<br>
+                                            Dịch vụ kh&#225;ch h&#224;ng:
+                                        </td>
+                                        <td class="whitetext">
+                                            <strong>0905.173.331</strong><br>
+                                            <strong>0932.472.310</strong><br>
+                                            <strong>0938.301.234</strong>
+                                        </td>
+                                        <td>
+                                            <a href="skype:fitdltn.danang?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:fitdlnn.danang?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:online.cskh?chat"><i class="iconT-skype"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3" class="city-name mauchudao">Chi Nh&#225;nh Cần Thơ</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 53%;">
+                                            Du lịch trong nước: <br>
+                                            Du lịch nước ngo&#224;i:<br>
+                                            Dịch vụ kh&#225;ch h&#224;ng:
+                                        </td>
+                                        <td class="whitetext">
+                                            <strong>0939 05 95 96</strong><br>
+                                            <strong>0903.114.664</strong><br>
+                                            <strong>0938.301.234</strong>
+                                        </td>
+                                        <td>
+                                            <a href="skype:fitdltn.cantho?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:fitdlnn.cantho?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:online.cskh?chat"><i class="iconT-skype"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3" class="city-name mauchudao">Chi Nh&#225;nh Hải Ph&#242;ng</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 53%;">
+                                            Du lịch trong nước: <br>
+                                            Du lịch nước ngo&#224;i:<br>
+                                            Dịch vụ kh&#225;ch h&#224;ng:
+                                        </td>
+                                        <td class="whitetext">
+                                            <strong>0936.900.095</strong><br>
+                                            <strong>0936.900.085</strong><br>
+                                            <strong>0938.301.234</strong>
+                                        </td>
+                                        <td>
+                                            <a href="skype:fitdltn.haiphong?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:fitdlnn.haiphong?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:online.cskh?chat"><i class="iconT-skype"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3" class="city-name mauchudao">Chi Nh&#225;nh Nha Trang</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 53%;">
+                                            Du lịch trong nước: <br>
+                                            Du lịch nước ngo&#224;i:<br>
+                                            Dịch vụ kh&#225;ch h&#224;ng:
+                                        </td>
+                                        <td class="whitetext">
+                                            <strong>&lrm;&lrm;093.1616.555</strong><br>
+                                            <strong>0988.608.286</strong><br>
+                                            <strong>0938.301.234</strong>
+                                        </td>
+                                        <td>
+                                            <a href="skype:fitdltn.nhatrang?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:fitdlnn.nhatrang?chat"><i class="iconT-skype"></i></a><br>
+                                            <a href="skype:online.cskh?chat"><i class="iconT-skype"></i></a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </li>
+                    </ul>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 col-sm-8 col-xs-8">
+            <div class="area-info">
+                <div class="f-right nav-top-mini hidden-lg hidden-md">
+                    <div class="i-menu text-right">
+                        <div><i class="fas fa-bars fa-lg"></i></div>
+                    </div>
+                    <ul class="nav-top" style="display: none;">
+                        <li class="hidden-sm hidden-xs">
+                            <a href="/">Du lịch</a>
+                            <ul class="sub-menu">
+                                <li><a href="/du-lich-viet-nam.aspx">Du lịch trong nước</a></li>
+                                <li><a href="/du-lich-nuoc-ngoai.aspx">Du lịch nước ngo&#224;i</a></li>
+                                <li><a href="https://travel.com.vn/Hotel/Destination"  rel="nofollow">Du lịch tự chọn</a></li>
+                                <li><a href="http://vscc.edu.vn/" target="_blank" rel="nofollow">Du học</a></li>
+                            </ul>
+                        </li>
+                        <li class="hidden-lg hidden-md"><a href="/du-lich-viet-nam.aspx">Du lịch trong nước</a></li>
+                        <li class="hidden-lg hidden-md"><a href="/du-lich-nuoc-ngoai.aspx">Du lịch nước ngo&#224;i</a></li>
+                        <li class="hidden-lg hidden-md"><a href="https://travel.com.vn/Hotel/Destination"  rel="nofollow">Du lịch tự chọn</a></li>
+                        <li class="hidden-lg hidden-md"><a href="http://vscc.edu.vn/" target="_blank" rel="nofollow">Du học</a></li>
+                        
+                        <li><a href="https://tripu.vn/vi/hotels" rel="nofollow">Kh&#225;ch sạn</a></li>
+                        <li class="hidden-sm hidden-xs">
+                            <a href="http://www.worldtrans.vn/" target="_blank" rel="nofollow">Vận chuyển</a>
+                            <ul class="sub-menu">
+                                <li><a href="http://car4rent.com/" target="_blank" rel="nofollow">Thu&#234; xe</a></li>
+                                <li><a href="http://www.worldtrans.vn/" target="_blank" rel="nofollow">V&#233; m&#225;y bay</a></li>
+                            </ul>
+                        </li>
+                        <li class="hidden-lg hidden-md"><a href="http://car4rent.com/" target="_blank" rel="nofollow">Thu&#234; xe</a></li>
+                        <li class="hidden-lg hidden-md"><a href="http://www.worldtrans.vn/" target="_blank" rel="nofollow">V&#233; m&#225;y bay</a></li>
+                        <li class="hidden-sm hidden-xs">
+                            <a href="/tin-tuc-du-lich.aspx">Tin tức</a>
+                            <ul class="sub-menu">
+                                <li><a href="/tin-tuc-du-lich.aspx">Tin tức</a></li>
+                                <li><a href="/du-lich-bang-hinh-anh.aspx">Cẩm nang du lịch</a></li>
+                                <li><a href="/kinh-nghiem-du-lich.aspx">Kinh nghiệm du lịch</a></li>
+                                <li><a href="/thong-tin-visa.aspx">Th&#244;ng tin visa</a></li>
+                            </ul>
+                        </li>
+                        <li class="hidden-lg hidden-md"><a href="/tin-tuc-du-lich.aspx">Tin tức</a></li>
+                        <li class="hidden-lg hidden-md"><a href="/du-lich-bang-hinh-anh.aspx">Cẩm nang du lịch</a></li>
+                        <li class="hidden-lg hidden-md"><a href="/kinh-nghiem-du-lich.aspx">Kinh nghiệm du lịch</a></li>
+                        <li class="hidden-lg hidden-md"><a href="/thong-tin-visa.aspx">Th&#244;ng tin visa</a></li>
+                        <li class="hidden-sm hidden-xs">
+                            <a href="#">Khuyến mại</a>
+                            <ul class="sub-menu">
+                                
+                                <li><a href="/tin-tuc-du-lich/tra-cham-0-lai-suat-cung-sacombank-v13244.aspx">Mua tour trả chậm 0%</a></li>
+                                <li><a href="/tin-tuc-du-lich/uu-dai-vnpay-v13243.aspx">Ưu đãi VNPay</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li class="hidden-lg hidden-md"><a href="/tin-tuc-du-lich/tra-cham-0-lai-suat-cung-sacombank-v13244.aspx">Mua tour trả chậm 0%</a></li>
+                        <li class="hidden-lg hidden-md"><a href="/tin-tuc-du-lich/uu-dai-vnpay-v13243.aspx">Mua tour trả chậm 0%</a></li>
+
+                        <li><a href="https://vietravelplus.com/" target="_blank" rel="nofollow">VietravelPlus</a></li>
+                        <li><a href="/lien-he.aspx">Li&#234;n hệ</a></li>
+                    </ul>
+                </div>
+                <div class="f-right h-line hidden-lg hidden-md">
+                    <img src="/Content/ThemeHe/img/h-line.png" alt="line">
+                </div>
+                <div class="f-right lang-info">
+                    <div class="obj pos-relative">
+                        <div class="dropdown-toggle" data-toggle="dropdown">
+                                <img src="/Content/ThemeHe/img/vn.png" alt="Vietnamese">
+                                <span style="padding-left: 5px;" class="hidden-xs">VN</span>
+                        </div>
+                        <ul class="dropdown-menu">
+                            <li style="border-bottom: 1px dashed #ccc;"><a href="https://travel.com.vn"><strong class="mauchudao">VN</strong> - Tiếng Việt</a></li>
+                            <li><a href="https://en.travel.com.vn"><strong class="mauchudao">EN</strong> - Tiếng Anh</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="f-right h-line">
+                    <img src="/Content/ThemeHe/img/h-line.png" alt="line">
+                </div>
+                <div class="f-right user-info">
+                    <div class="obj pos-relative">
+                        <img src="/Content/ThemeHe/img/user-s.png" alt="Tài khoản" class="dropdown-toggle" data-toggle="dropdown">
+                        <ul class="dropdown-menu">
+                                <li style="border-bottom: 1px dashed #ccc;">
+                                    <a id="btDangNhap" href="#" data-toggle="modal" data-target="#dangnhap">Đăng nhập</a>
+                                </li>
+                                <li><a href="http://vietravelplus.com/dang-ky-hoi-vien" target="_blank">Đăng k&#253;</a></li>
+                        </ul>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <!-- start voucher 2020 -->
+                
+                <!-- start voucher 2020 -->
+                <!-- start voucher 2020 them class hidden-xs -->
+                <div class="f-right h-line hidden-xs">
+                    <img src="/Content/ThemeHe/img/h-line.png" alt="line">
+                </div>
+
+                
+    <div class="f-right cart-info  hidden-xs">
+        <div class="obj pos-relative">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="/Content/ThemeHe/img/v/i-viewed.png" alt="line">
+                <div class="frame-cart">
+                    <div class="circle-num">
+                        <span class="num">0</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+ <!-- them class hidden-xs -->
+
+                <div class="f-right hidden-xs">
+                    <img src="/Content/ThemeHe/img/h-line.png" alt="line">
+                </div>
+                <!-- start voucher 2020  them class hidden-xs -->
+
+
+
+                <div class="f-right search-mini hidden-xs">
+                    <div class="obj pos-relative">
