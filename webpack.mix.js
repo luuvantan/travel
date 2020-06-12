26a0f0c291d89af28acdc831f32379e4a1a118c8
TanLV <tanlv@fabbi.io>
2020-06-12 14:46:26 +0700

    auth-laravel

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/menu.js', 'public/js')
+.js('resources/js/menu.js', 'public/js')
     .js('resources/js/posts/create.js', 'public/js/posts')
     .js('resources/js/posts/index.js', 'public/js/posts')
     .sass('resources/sass/app.scss', 'public/css')
