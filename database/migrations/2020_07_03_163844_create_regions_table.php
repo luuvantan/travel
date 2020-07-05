1c3d7288927e31a6569bc8628110f983b81e2953
tanluu97 <tanthptdt@gmail.com>
2020-07-05 23:42:29 +0700

    add more migrate

--- database/migrations/2020_07_03_163844_create_regions_table.php
+++ database/migrations/2020_07_03_163844_create_regions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateRegionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('regions', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
             $table->string('name')->nullable();
-            $table->string('category_id');
-            $table->string('provincial_id');
-            $table->string('place')->nullable();
-            $table->string('title')->nullable();
-            $table->longText('content')->nullable();
-            $table->string('url_img')->nullable();
             $table->timestamps();
         });
     }
@@ -34,6 +27,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('regions');
     }
 }
